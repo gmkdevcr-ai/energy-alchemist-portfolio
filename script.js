b3500a924c54e5435b36d336389014e6f549e7cd
gmkdevcr-ai <gmk.dev.cr@gmail.com>
2026-05-27 04:04:58 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+
+  event.preventDefault();
+
+  alert("✨ Tu sesión fue reservada correctamente ✨");
+
+});
