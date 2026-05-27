b55b6269a9803ccb397c87b585ca5f3945b418be
gmkdevcr-ai <gmk.dev.cr@gmail.com>
2026-05-27 06:12:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,12 @@ window.addEventListener("scroll", function() {
   });
 
 });
+const menuToggle = document.querySelector(".menu-toggle");
+
+const navMenu = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function() {
+
+  navMenu.classList.toggle("active");
+
+});
